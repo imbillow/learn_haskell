51f8e242515948adaaf9bac28de0e30a82c083a3
billow <billow.fun@gmail.com>
2020-12-22 17:23:27 +0800

    777

--- upenn_cis194/07/JoinList.hs
+++ upenn_cis194/07/JoinList.hs
@@ -1,5 +1,10 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module JoinList where
 
+import Buffer
+import Editor
+import Scrabble
 import Sized
 
 data JoinList m a
@@ -56,15 +61,59 @@ dropJ x (Append _ xs ys) = dropJ x xs +++ dropJ (x - sx) ys
   where
     sx = getSize' $ tag xs
 
-takeJ :: (Sized b, Monoid b) =>
-  Int -> JoinList b a -> JoinList b a
+takeJ ::
+  (Sized b, Monoid b) =>
+  Int ->
+  JoinList b a ->
+  JoinList b a
 takeJ x zs
   | x <= 0 = Empty
   | x >= sz = zs
   where
     sz = getSize' $ tag zs
-takeJ x (Append _ xs ys) = takeJ x xs +++ takeJ (x-sx) ys
+takeJ x (Append _ xs ys) = takeJ x xs +++ takeJ (x - sx) ys
   where
     sx = getSize' $ tag xs
 
 -- 3
+
+scoreLine :: String -> JoinList Score String
+scoreLine str = Single (scoreString str) str
+
+-- 4
+
+instance Monoid b => Semigroup (JoinList b a) where
+  (<>) = (+++)
+
+instance Monoid b => Monoid (JoinList b a) where
+  mempty = Empty
+
+instance Buffer (JoinList (Score, Size) String) where
+  toString Empty = ""
+  toString (Single _ s) = s
+  toString (Append _ xs ys) = toString xs ++ toString ys
+
+  fromString = mconcat . map (\l -> Single (scoreString l, Size 1) l) . lines
+
+  line = indexJ
+
+  replaceLine n l b = postfix +++ fromString l +++ suffix
+    where
+      postfix = takeJ n b
+      suffix = dropJ (n + 1) b
+
+  numLines = getSize . size . tag
+  value = getScore . score_ . tag
+
+main =
+  runEditor editor buffer
+  where
+    buffer :: JoinList (Score, Size) String
+    buffer =
+      fromString $
+        unlines
+          [ "This buffer is for notes you don't want to save, and for",
+            "evaluation of steam valve coefficients.",
+            "To load a different file, type the character L followed",
+            "by the name of the file."
+          ]
