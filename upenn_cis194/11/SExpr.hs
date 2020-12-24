f829f3ad27de47328d75dda843622e254ffcd590
billow <billow.fun@gmail.com>
2020-12-24 17:10:47 +0800

    11.comp

--- upenn_cis194/11/SExpr.hs
+++ upenn_cis194/11/SExpr.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+
 {- CIS 194 HW 11
    due Monday, 8 April
 -}
@@ -6,26 +8,27 @@ module SExpr where
 
 import AParser
 import Control.Applicative
+import Data.Char
 
 ------------------------------------------------------------
 --  1. Parsing repetitions
 ------------------------------------------------------------
 
 zeroOrMore :: Parser a -> Parser [a]
-zeroOrMore p = undefined
+zeroOrMore = many
 
 oneOrMore :: Parser a -> Parser [a]
-oneOrMore p = undefined
+oneOrMore p = (:) <$> p <*> zeroOrMore p
 
 ------------------------------------------------------------
 --  2. Utilities
 ------------------------------------------------------------
 
 spaces :: Parser String
-spaces = undefined
+spaces = zeroOrMore $ satisfy isSpace
 
 ident :: Parser String
-ident = undefined
+ident = (:) <$> satisfy isAlpha <*> zeroOrMore (satisfy isAlphaNum)
 
 ------------------------------------------------------------
 --  3. Parsing S-expressions
@@ -45,3 +48,12 @@ data SExpr
   = A Atom
   | Comb [SExpr]
   deriving (Show)
+
+atom :: Parser Atom
+atom = spaces *> (N <$> posInt) <|> (I <$> ident)
+
+comb :: Parser [SExpr]
+comb = spaces *> char '(' *> zeroOrMore (spaces *> parseSExpr) <* (spaces *> char ')')
+
+parseSExpr :: Parser SExpr
+parseSExpr = (A <$> atom) <|> (Comb <$> comb)
