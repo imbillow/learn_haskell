87a525a2ac7238963e856eb5b014d257badc5b09
billow <billow.fun@gmail.com>
2020-12-26 17:54:53 +0800

    fp FileIO

--- fp-course/src/Course/FileIO.hs
+++ fp-course/src/Course/FileIO.hs
@@ -1,15 +1,15 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Course.FileIO where
 
-import Course.Core
 import Course.Applicative
-import Course.Monad
+import Course.Core
 import Course.Functor
 import Course.List
+import Course.Monad
 
 {-
 
@@ -82,49 +82,50 @@ the contents of c
 -- Given the file name, and file contents, print them.
 -- Use @putStrLn@.
 printFile ::
-  FilePath
-  -> Chars
-  -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+  FilePath ->
+  Chars ->
+  IO ()
+printFile p s =
+  putStrLn ("============ " ++ p)
+    >> putStrLn s
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
-  List (FilePath, Chars)
-  -> IO ()
+  List (FilePath, Chars) ->
+  IO ()
 printFiles =
-  error "todo: Course.FileIO#printFiles"
+  mapM_ (uncurry printFile)
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
-  FilePath
-  -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+  FilePath ->
+  IO (FilePath, Chars)
+getFile p =
+  (,) p <$> readFile p
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
-  List FilePath
-  -> IO (List (FilePath, Chars))
+  List FilePath ->
+  IO (List (FilePath, Chars))
 getFiles =
-  error "todo: Course.FileIO#getFiles"
+   mapM getFile
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@, @lines@, and @printFiles@.
 run ::
-  FilePath
-  -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+  FilePath ->
+  IO ()
+run p =
+  readFile p >>= getFiles . lines >>= printFiles
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
 main =
-  error "todo: Course.FileIO#main"
+  getArgs >>= mapM_ run
 
 ----
 
@@ -132,3 +133,9 @@ main =
 -- ? `sequence . (<$>)`
 -- ? `void . sequence . (<$>)`
 -- Factor it out.
+
+mapM :: Monad m => (a -> m b) -> List a -> m (List b)
+mapM f = sequence . (<$>) f
+
+mapM_ :: Monad m => (a -> m b) -> List a -> m ()
+mapM_ f = void . mapM f
